a56d3aa9547c74ae65ae3091937e28fb9701d8d1
NatiHermida <hermidaltamarnati@gmail.com>
2025-02-23 11:51:02 +0100

    cambiocarpetaconfig

--- config/config_sample.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("HOST", "servidor");
-define("DB_USER", "usuario");
-define("DB_PASS", "contraseña");
-define("DB_DATABASE", "contraseña");
-define("JWT_SECRET_KEY","contraseña");//clave secreta para el token
-?>
